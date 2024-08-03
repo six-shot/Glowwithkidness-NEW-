49cc3f66dc7ced98f51eb68de921eb2fbce60721
six-shot <okhuomonajayi54@gmail.com>
2024-08-03 21:48:44 +0000

    fix:landing page

--- src/modules/home/features/about us/index.jsx
+++ src/modules/home/features/about us/index.jsx
@@ -14,53 +14,30 @@ export default function About() {
         </div>
         <div className="sm:col-span-3">
           <div className="flex gap-2 items-center">
-            <h5 className="">EXPERIENCE WELLNEZ</h5>
+            <h5 className="">EXPERIENCE GLOWWITHKINDNESS</h5>
             <h5 className="bg-primary px-5 py-1">25 YEARS</h5>
           </div>
           <h3 className="text-[50px] ">DISCOVER A NEW</h3>
 
-          <h5 className="mt-2">
-            We think your heair should look and refreshed matter <br /> your
-            lifestyle Wellnez.
+          <h5 className="mt-2 sm:w-[600px]">
+            We are thrilled that you have chosen us for your beauty services,
+            and we hope you become one of our valued regular clients over your
+            next several appointments.
           </h5>
-          <div className="grid grid-cols-2 gap-[10%] mt-[10%]">
-            <div className="col-span-1 hover:bg-primary p-3">
-              <div className="w-full h-[200px]  border border-dashed border-primary hover:border-white flex relative">
-                <div className="absolute -top-[25%] flex items-center w-full justify-center ">
-                  <div className="bg-white hover:bg-primary p-2 rounded-full">
-                    <div className="w-[80px] h-[80px] bg-primary rounded-full flex justify-center items-center">
-                      <PiHairDryer className="text-[40px] text-white" />
-                    </div>
-                  </div>
-                </div>
-                <div className="flex flex-col justify-center w-full items-center px-[8%]">
-                  <h5 className="text-black text-[22px]">Body Treatments</h5>
-                  <p className="text-black/60 text-sm text-center mt-5">
-                    There many variations passages Ipsum availle majority have
-                    suffered in some.
-                  </p>
-                </div>
-              </div>
-            </div>
-            <div className="col-span-1 hover:bg-primary p-3">
-              <div className="w-full h-[200px]  border border-dashed border-primary hover:border-white flex relative">
-                <div className="absolute -top-[25%] flex items-center w-full justify-center ">
-                  <div className="bg-white hover:bg-primary p-2 rounded-full">
-                    <div className="w-[80px] h-[80px] bg-primary rounded-full flex justify-center items-center">
-                      <PiHairDryer className="text-[40px] text-white" />
-                    </div>
-                  </div>
-                </div>
-                <div className="flex flex-col justify-center w-full items-center px-[8%]">
-                  <h5 className="text-black text-[22px]">Body Treatments</h5>
-                  <p className="text-black/60 text-sm text-center mt-5">
-                    There many variations passages Ipsum availle majority have
-                    suffered in some.
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
+          <p className="mt-4 sm:w-[600px]">
+            We offer hair styling, makeup, and more. Our team of trained,
+            qualified, and licensed professionals pride ourselves on our ability
+            to provide a variety of the highest quality services possible that
+            are designed to create a look that&apos;s perfect for you, make you
+            look and feel your best at competitive prices while maintaining the
+            highest standard of hygiene and cleanliness within our facility.
+          </p>
+          <p className="mt-4 sm:w-[600px]">
+            Kindly read our <span className="text-primary">terms and conditions</span> to better
+            understand how we operate. We hope you&apos;ll visit us soon and
+            experience the difference our salon can make.
+          </p>
+        
         </div>
       </div>
     </div>
