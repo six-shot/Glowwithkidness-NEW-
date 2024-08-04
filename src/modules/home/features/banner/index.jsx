598eb4119cd2d839a06e3bdcfa77d5321aa0f6e4
six-shot <okhuomonajayi54@gmail.com>
2024-08-04 01:04:08 +0000

    fix:landing page

--- src/modules/home/features/banner/index.jsx
+++ src/modules/home/features/banner/index.jsx
@@ -38,7 +38,7 @@ export default function Banner() {
     <div>
       <div className="px-[5%] sm:py-[3%]  py-[5%]">
         <div className="">
-          <Marquee className="mq" direction="right" speed={60}>
+          <Marquee className="mq" direction="left" speed={60}>
             {styles.map((style) => (
               <div className="sm:w-[400px] w-[300px]  sm:h-[300px] h-[250px]  flex flex-col justify-between  p-[4%]">
                 <div className="col-span-1 h-full bg-primary px-[5%] py-[8%] ">
--- src/modules/home/features/footer/index.jsx
+++ src/modules/home/features/footer/index.jsx
@@ -6,7 +6,7 @@ import { FiPhone } from "react-icons/fi";
 import { MdOutlineEmail } from "react-icons/md";
 export default function Footer() {
   return (
-    <div className="relative w-full sm:h-[110vh] h-[170vh]" id="home">
+    <div className="relative w-full sm:h-[110vh] h-[150vh]" id="home">
       <img
         className="absolute  object-cover left-0 w-full h-full z-10"
         src="/footer.jpg"
