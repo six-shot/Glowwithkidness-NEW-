5a04dda080b7e441fc4200a8d2dc3c4c818215f3
six-shot <okhuomonajayi54@gmail.com>
2024-08-17 16:21:58 +0000

    fix

--- src/modules/home/pages/booking/index.jsx
+++ src/modules/home/pages/booking/index.jsx
@@ -5,12 +5,12 @@ import Navbar from "../../features/navbar";
 
 export default function Booking() {
   return (
-    <div>
+    <div className="">
       <Navbar />
-      <div className="max-w-[1440px] mx-auto pt-[30vh]">
+      <div className="max-w-[1440px] mx-auto pt-[30vh] overflow-y-hidden ">
         <div className="px-[5%]">
           <h4 className="text-[30px] font-
-          semibold mb-10 text-center">Book An Appointment </h4>
+          semibold text-center">Book An Appointment </h4>
           <Book />
         </div>
       </div>
